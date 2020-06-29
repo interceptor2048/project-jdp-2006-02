02b24a33eaa25bbea963abaaa71860a123f875b6
lisowski90 <lisowski1990@gmail.com>
2020-06-30 01:22:03 +0200

    USER ENTITY CREATE

--- src/main/java/com/kodilla/ecommercee/domain/User.java
+++ src/main/java/com/kodilla/ecommercee/domain/User.java
@@ -4,13 +4,35 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
+@Entity(name = "T_USER")
 public class User {
 
+    @Id
+    @GeneratedValue
+    @Column(name = "USER_ID")
     private Long id;
+
+    @Column(name = "USERNAME")
     private String username;
+
+    @Column(name = "STATUS")
     private String status;
+
+    @Column(name = "USER_KEY")
     private String userKey;
+
+    @ManyToMany(cascade = CascadeType.ALL, targetEntity = com.kodilla.ecommercee.domain.Product.class)
+    @JoinTable(
+            name = "CART",
+            joinColumns = {@JoinColumn(name = "USER_ID", referencedColumnName = "USER_ID")},
+            inverseJoinColumns = {@JoinColumn(name = "PRODUCT_ID", referencedColumnName = "PRODUCT_ID")}
+    )
+    private List<Product> productList = new ArrayList<>();
 }
