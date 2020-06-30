cfb6db07213bd3e2c97eaa1ff8e8b250b03ad214
Ready2112 <kzak.air@gmail.com>
2020-06-30 23:56:08 +0200

    create GroupMapper, GroupNotFoundException and implement GroupControler final method

--- src/main/java/com/kodilla/ecommercee/controller/GroupController.java
+++ src/main/java/com/kodilla/ecommercee/controller/GroupController.java
@@ -1,8 +1,12 @@
 package com.kodilla.ecommercee.controller;
 
+import com.kodilla.ecommercee.domain.Group;
 import com.kodilla.ecommercee.domain.GroupDto;
+import com.kodilla.ecommercee.exception.GroupNotFoundException;
+import com.kodilla.ecommercee.mapping.GroupMapper;
+import com.kodilla.ecommercee.service.GroupDbService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import java.util.Arrays;
 import java.util.List;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
@@ -11,26 +15,30 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @CrossOrigin("*")
 public class GroupController {
 
+    @Autowired
+    GroupDbService groupDbService;
+
+    @Autowired
+    GroupMapper groupMapper;
+
     @RequestMapping(method = RequestMethod.GET, value = "getGroups")
     public List<GroupDto> getGroups(){
-        return Arrays.asList(
-                new GroupDto(1L ,"test group one"),
-                new GroupDto(2L, "Test group two"));
+        return groupMapper.mapToGroupDtoList(groupDbService.getAllGroups());
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "createGroup", consumes = APPLICATION_JSON_VALUE)
-    public GroupDto createGroup(@RequestBody GroupDto groupDto){
-        return groupDto;
+    public Group createGroup(@RequestBody GroupDto groupDto){
+        return groupDbService.saveGroup(groupMapper.mapToGroup(groupDto));
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "getGroup")
-    public GroupDto getGroup(@RequestParam Long id){
-        return new GroupDto(id,"test group");
+    public GroupDto getGroup(@RequestParam Long id) throws GroupNotFoundException {
+        return  groupMapper.mapToGroupDto(groupDbService.getGroup(id).orElseThrow(GroupNotFoundException::new));
     }
 
     @RequestMapping(method = RequestMethod.PUT, value = "updateGroup", consumes = APPLICATION_JSON_VALUE)
     public GroupDto updateGroup(@RequestBody GroupDto groupDto){
-        return groupDto;
+        return groupMapper.mapToGroupDto(groupDbService.saveGroup(groupMapper.mapToGroup(groupDto)));
     }
 
 }
