1a5d3c9d2c8f8f51b78c1a36b0354bde0cfcc8b5
hubertgorczynski <hubert.gorczynski378@gmail.com>
2020-06-30 13:53:01 +0200

    Implementation of entity Order, create OrderRepository and OrderDbService with methods

--- src/main/java/com/kodilla/ecommercee/domain/Order.java
+++ src/main/java/com/kodilla/ecommercee/domain/Order.java
@@ -4,11 +4,21 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity(name = "ORDERS")
 public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @Column(name = "USER_ID")
     private Long userId;
+
+    @Column(name = "STATUS")
     private String status;
 }
