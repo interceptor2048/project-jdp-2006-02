658921dc198cfc0215dec3dee02e6c770b6ea021
GrzegorzWoloszyn <pgrzegorz.woloszyn@gmail.com>
2020-07-07 22:34:57 +0200

    code review, method changed

--- src/main/java/com/kodilla/ecommercee/controller/CartController.java
+++ src/main/java/com/kodilla/ecommercee/controller/CartController.java
@@ -1,8 +1,14 @@
 package com.kodilla.ecommercee.controller;
 
-import com.kodilla.ecommercee.domain.CartDto;
-import com.kodilla.ecommercee.domain.OrderDto;
-import com.kodilla.ecommercee.domain.ProductDto;
+import com.kodilla.ecommercee.domain.*;
+import com.kodilla.ecommercee.exception.UserNotFoundException;
+import com.kodilla.ecommercee.mapping.ProductMapper;
+import com.kodilla.ecommercee.mapping.UserMapper;
+import com.kodilla.ecommercee.service.OrderDbService;
+import com.kodilla.ecommercee.service.ProductDbService;
+import com.kodilla.ecommercee.service.UserDbService;
+import org.aspectj.weaver.ast.Or;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -15,6 +21,21 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @CrossOrigin(origins = "*")
 public class CartController {
 
+    @Autowired
+    private ProductDbService productDbService;
+
+    @Autowired
+    private OrderDbService orderDbService;
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Autowired
+    private UserDbService userDbService;
+
+    @Autowired
+    private ProductMapper productMapper;
+
     @RequestMapping(method = RequestMethod.POST, value = "createEmptyCart")
     public CartDto createEmptyCart(@RequestParam Long userId) {
         return new CartDto(1L, userId);
@@ -22,20 +43,28 @@ public class CartController {
 
     @RequestMapping(method = RequestMethod.GET, value = "getProductsFromCart")
     public List<ProductDto> getProductsFromCart() {
-        return new ArrayList<>();
+        return productMapper.mapToProductDtoList(productDbService.getAllProducts());
+
     }
 
     @RequestMapping(method = RequestMethod.PUT, value = "addProductToCart", consumes = APPLICATION_JSON_VALUE)
-    public CartDto addProductToCart(@RequestBody ProductDto productDto) {
-        return new CartDto(1L, 2L);
+    public List<Product> addProductToCart(@RequestBody ProductDto productDto) {
+        List<Product> products = new ArrayList<>();
+        products.add(productMapper.mapToProduct(productDto));
+        return products;
     }
 
     @RequestMapping(method = RequestMethod.DELETE, value = "deleteProductFromCart")
     public void deleteProductFromCart(@RequestParam Long productId) {
+        productDbService.deleteProduct(productId);
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "createOrder")
-    public OrderDto createOrder(@RequestParam Long userId) {
+    public OrderDto createOrder(@RequestParam Long userId) throws UserNotFoundException {
+//       return orderDbService.saveOrder(new Order(1L, userDbService.getUser(userId).orElseThrow(UserNotFoundException::new), "done"));
+//       return orderDbService.saveOrder(new Order(1L, userMapper.mapToUser(userMapper.mapToUserDto(userDbService.getUser(userId))), "done"));
+
+
         return new OrderDto(1L, userId, "status");
     }
 }
